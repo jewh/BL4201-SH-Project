e50d71d9f1367758872fb25775caa2f4f35529b7
James Hammond <33940814+jewh@users.noreply.github.com>
2019-11-20 20:31:13 +0000

    Added a negative control random matrix for testing too.

--- Network Generator.py
+++ Network Generator.py
@@ -31,42 +31,14 @@ def non_unitary_heaviside(x1, x2):
     return out
 
 
-class InteractionNetwork:
-
-    def __init__(self, nodes, links, alpha, beta):
-        self.nodes = nodes
-        self.links = links
-        self.alpha = alpha
-        self.beta = beta
-
-    def create_network(self):
-        # Generate a random graph with n nodes and m directed edges:
-        network = nx.gnm_random_graph(self.nodes, self.links, directed=True)
-        for (i, j) in network.edges():
-            network.edges[i, j]['weight'] = rd.betavariate(self.alpha, self.beta)*(-1)**(rd.choice((1, 2)))
-        jacobian = nx.to_numpy_matrix(network, dtype=float)
-        return jacobian
-
-    # ***This function appears to work as intended, maybe need to alter the distribution of interaction strengths though***
-    # Now create function for visualising the network structure
-    def true_network(self):
-        jacobian = self.create_network()
-        g = nx.from_numpy_matrix(jacobian, create_using=nx.DiGraph())
-        plt.subplot(111)
-        nx.draw(g, with_labels=True, font_weight='bold', pos=nx.circular_layout(g))
-        plt.suptitle("Network on {0} nodes, with {1} links".format(self.nodes, self.links))
-        plt.title("With interactions given by a beta distribution where alpha = {0}, beta = {1}".format(self.alpha, self.beta))
-        plt.show()
-
-
-    # Now create class of network equillibrium states, for given network types.
+# Now create class of network equillibrium states, for given network types.
 
 
 class EvolvedNetwork:
 
     # set network parameters as input parameters
 
-    def __init__(self, kind, nodes, links, noise, iterations, time=1000, alpha=5.0, beta=5.0):
+    def __init__(self, kind, nodes, links, noise, iterations, instance, time=1000, alpha=5.0, beta=5.0):
         self.kind = kind   # kind of network - e.g. one with layers, one with extinction, etc.
         self.nodes = nodes  # number of network nodes
         self.links = links  # number of network edges
@@ -75,6 +47,7 @@ class EvolvedNetwork:
         self.alpha = alpha    # describes network weight distribution
         self.beta = beta      # describes network weight distribution
         self.time = time      # length of system evolution
+        self.instance = instance # creates different instances, hopefully to create different networks for same parameters
 
     def create_network(self):
         # Generate a random graph with n nodes and m directed edges:
@@ -97,13 +70,18 @@ class EvolvedNetwork:
 
     def evolve_system(self):
         jacobian = self.create_network()
-        print(jacobian)
+        print('pass')
         # Create a 'population' vector describing the population at time t, which will be our output
         out = np.zeros((self.nodes, self.iterations), dtype=float)
+        # And a positive control network too.
+        control = np.zeros((self.nodes, self.iterations), dtype=float)
+        # And a totally random, negative control
+        neg_control = np.zeros((self.nodes, self.iterations), dtype=float)
         # Create an initial state for this population
         for i in range(0, self.nodes):
             for j in range(0, self.iterations):
                 out[i, j] = 10.0  # Set to an arbitrary constant value for now, can change later, just want reproducibility
+                control[i, j] = 10.0
         t = 0  # create time counter
         # Now evolve the system, for different kinds of network:
         if self.kind == 'extinction':
@@ -111,22 +89,35 @@ class EvolvedNetwork:
                 t = t + 1
                 for i in range(0, self.nodes):
                     for j in range(0, self.iterations):
+                        neg_control[i, j] = rd.uniform(0, 100) # creates a random negative control network for testing
                         if out[i, j] == 0:  # This keeps nodes extinct
                             break
                         else:
                             for k in range(0, self.nodes):
                                 out[i, j] = non_unitary_heaviside(out[i, j] + noisy_interaction(jacobian[k, i], out[k, j], self.noise)*out[k, j], 0.0) # heaviside function creates extinction
+                                control[i, j] = control[i, j] + noisy_interaction(jacobian[k, i], out[k, j], self.noise) * out[k, j]
+            # Now create .txt outputs for these networks.
+            file_network = np.savetxt(
+                f"Outputs - Extinction Networks\{self.kind} network structure with n{self.nodes} L{self.links} N{self.noise} I{self.iterations} in{self.instance}.txt",
+                jacobian)
+            file_out = np.savetxt(
+                f"Outputs - Extinction Networks\{self.kind} network n{self.nodes} L{self.links} N{self.noise} I{self.iterations} in{self.instance}.txt", out)
+            file_control = np.savetxt(
+                f"Outputs - Extinction Networks\control {self.kind} network n{self.nodes} L{self.links} N{self.noise} I{self.iterations} in{self.instance}.txt",
+                control)
+            file_neg_control = np.savetxt(
+                f"Outputs - Extinction Networks\ negative control {self.kind} network n{self.nodes} L{self.links} N{self.noise} I{self.iterations} in{self.instance}.txt",
+                neg_control)
         else:
             print('ERROR: {} is not a valid kind of network!'.format(self.kind))
-        return out
-
-    def output_file(self):
-        file = np.savetxt("Outputs - Extinction Networks\{0} network n{1} L{2} N{3} I{4}.txt".format(self.kind, self.nodes, self.links, self.noise, self.iterations), self.evolve_system())
 
 
-out = EvolvedNetwork('extinction', 5, 10, 4.0, 10)
-print(out.evolve_system())
-print(out.create_network())
-# print(out.network())
-out.output_file()
+#Now generate data for analysis
+number_networks = 20
+for i in range(0, number_networks):
+    out = EvolvedNetwork('extinction', 6, 15, 4.0, 1000, i)
+    out.evolve_system()
+
+
+
 
