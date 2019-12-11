49c4c92e7dd20c73ebaedee0ac8427cef666cbfb
James Hammond <33940814+jewh@users.noreply.github.com>
2019-12-11 11:26:39 +0000

    Removed the troubleshooting message

--- Network Generator.py
+++ Network Generator.py
@@ -113,7 +113,6 @@ class EvolvedNetwork:
                             for k in range(0, self.nodes):
                                 out[i, j] = bound(non_unitary_heaviside(out[i, j] + noisy_interaction(jacobian[k, i], out[k, j], self.noise)*out[k, j], 0.0)) # heaviside function creates extinction
                                 control[i, j] = bound(control[i, j] + noisy_interaction(jacobian[k, i], out[k, j], self.noise) * out[k, j])
-                                print("------------it works fam-------------")
             # Now create .txt outputs for these networks.
             file_network = np.savetxt(
                 f"Outputs - Extinction Networks\{self.kind} network structure with n{self.nodes} L{self.links} N{self.noise} I{self.iterations} in{self.instance}.txt",
